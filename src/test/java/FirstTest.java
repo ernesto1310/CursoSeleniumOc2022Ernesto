f33893b0862b462afb5750e387d29cda771276c2
eanaya <ernesto.anaya@perficient.com>
2022-10-27 16:18:21 -0500

    FirstTest

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void f() {
+        ChromeOptions chromeOpt = new ChromeOptions();
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver(chromeOpt);
+
+        driver.get("https://www.google.com/");
+    }
+}
